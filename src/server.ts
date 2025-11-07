ed3f29a3f65743876643ae7a1038f4928e884f66
Luan Hubner <luanhubner.pro@gmail.com>
2025-11-07 14:32:05 -0400

    chore(deps): initial dependencies

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(`server running on port ${process.env.APP_PORT} 🚀`)
+})
